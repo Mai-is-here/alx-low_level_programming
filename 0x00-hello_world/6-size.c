071861ae7be8d725a0af635e56c4502fafff3808
Mai-is-here <M.Hassan70190@yahoo.com>
2023-02-20 21:19:22 -0800

    calculating sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print out the size of various types on computer
+ * Return: 0 if code runs without errors
+ */
+int main(void)
+{
+	printf("Size of a char: %2d byte(s)\n", sizeof(char));
+	printf("Size of an int: %2d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %2d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %2d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %2d byte(s)", sizeof(float));
+	return (0);
+}
