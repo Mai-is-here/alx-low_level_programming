e60ba491a615d493a0d48aaade9c652ccbe0a0d6
Mai-is-here <M.Hassan70190@yahoo.com>
2023-02-20 19:07:27 -0800

    making c files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void)/*this function shall print a string*/
-	/*this function prints strings*/
+/*this function will display a string*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
