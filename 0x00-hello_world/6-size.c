23cbcb642cd13f6b837bee0f31e42b18aa1d824a
Mai-is-here <M.Hassan70190@yahoo.com>
2023-02-20 21:30:57 -0800

    added stderr

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,5 +11,6 @@ int main(void)
 	printf("Size of a long int:%2d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int:%2d byte(s)\n", sizeof(long long int));
 	printf("Size of a float:%2d byte(s)", sizeof(float));
+	printf(stderr);
 	return (0);
 }
