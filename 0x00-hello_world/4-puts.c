51a867c36f86d513532cb767adb2cc372342d4ff
Mai-is-here <M.Hassan70190@yahoo.com>
2023-02-20 18:57:58 -0800

    trying to fix doc;

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,7 @@
 #include <string.h>
 
 int main(void)/*this function shall print a string*/
+	/*this function prints strings*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
