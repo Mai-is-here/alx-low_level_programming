6e67221e2cba251c1d8b3bb5480619e02dca7147
Mai-is-here <M.Hassan70190@yahoo.com>
2023-02-23 17:52:30 -0800

    uppercase

---
+++ 0x04-more-functions_nested_loops/0-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", _c, isupper(c));
+	c = 'a';
+	printf("%c: %d\n", _c, isupper(c));
+	return (0);
+}
+
